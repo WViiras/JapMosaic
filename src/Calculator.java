02930643ce136cdfb646e2cfc201c14ef09b33ba
WV <WViiras@users.noreply.github.com>
2017-04-14 22:27:13 +0300

    main is now in Calculator class

--- src/Calculator.java
+++ src/Calculator.java
@@ -23,6 +23,15 @@ public class Calculator {
         System.out.println("numberMatrix");
         printMatrix(numberMatrix);
     }
+    
+    public static void main(String[] args) {
+        Calculator m59 = new Calculator("59");
+        m59.solve();
+        Calculator m60 = new Calculator("60");
+        m60.solve();
+        Calculator mF = new Calculator("F");
+        mF.solve();
+    }
 
     private List<String> getFile(String filename) {
         List<String> lines = new ArrayList<>();
